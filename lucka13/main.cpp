0cdd79cf63952c8b042be9ebec842cd7d952fd6d
Daniel Sävström <daniel.savstrom@king.com>
2020-04-14 13:25:13 +0200

    Added timer

--- lucka13/main.cpp
+++ lucka13/main.cpp
@@ -4,11 +4,16 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
+	auto startTime = std::clock();
+	
 	auto numSteps = countSteps({31, 39}, 1362);
-	std::cout << "Number of steps: " << numSteps << std::endl;
-
 	auto numVisited = fiftySteps(1362);
+	
+	double duration = ( std::clock() - startTime ) / static_cast<double>(CLOCKS_PER_SEC);
+
+	std::cout << "Number of steps: " << numSteps << std::endl;
 	std::cout << "Unique visits: " << numVisited << std::endl;
+	std::cout << "Time: " << duration << std::endl;
 
 	return EXIT_SUCCESS;
 }
